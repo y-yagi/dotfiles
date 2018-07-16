cc1178d3b377c135f78dc1595c40facbf0f76a41
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-07-16 14:43:12 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# tools
+
+go get -f -u github.com/mattn/memo
+go get -f -u github.com/y-yagi/jpcal
+go get -f -u github.com/y-yagi/jptenki
+go get -f -u github.com/y-yagi/myrepo
+go get -f -u github.com/y-yagi/blogrep
+go get -f -u github.com/y-yagi/multi-ag
+go get -f -u github.com/y-yagi/egose
+go get -f -u github.com/y-yagi/goto
+go get -f -u github.com/y-yagi/tomato
+
+go get -f -u github.com/davecheney/httpstat
+
