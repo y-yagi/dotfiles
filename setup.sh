590aecb49712248f50b763f1d107de8ea565bede
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-08-31 13:14:01 +0900

    Fix path

--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ go get -f -u github.com/y-yagi/blogrep
 go get -f -u github.com/y-yagi/multi-ag
 go get -f -u github.com/y-yagi/egose
 go get -f -u github.com/y-yagi/goto
-go get -f -u github.com/y-yagi/tomato
+go get -f -u github.com/y-yagi/tomato/cmd/tomato
 go get -f -u github.com/y-yagi/open-hub
 go get -f -u github.com/y-yagi/github-trending
-go get -f -u github.com/y-yagi/weatherer
+go get -f -u github.com/y-yagi/weatherer/cmd/weatherer
 
 go get -f -u github.com/davecheney/httpstat
